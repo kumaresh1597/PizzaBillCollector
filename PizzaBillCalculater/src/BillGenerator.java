2b7814a0ff617488fba26397686b60d750ea9ee8
Kumaresh <kumaresh.1597@gmail.com>
2023-04-05 00:01:18 +0530

    Delete PizzaBillCalculater directory

--- PizzaBillCalculater/src/BillGenerator.java
+++
@@ -1,8 +0,0 @@
-
-//Pizza Bill generate the total bill for purchase the pizza
-public class BillGenerator {
-    public static void main(String[] args) throws Exception {
-        //Call the class GUI1 
-        GUI1 frame = new GUI1();
-    }
-}
--- PizzaBillCalculater/src/DeluxPizza.java
+++
@@ -1,9 +0,0 @@
-// Delux pizza extend the class Pizza
-public class DeluxPizza extends Pizza{
-   //Delux pizza inbuilt add cheese and toppings
-   DeluxPizza(boolean isVeg){
-    super(isVeg); // calling the parent class constructor with parameter
-    addExtraCheese();
-    addToppings();
-   }
-}
